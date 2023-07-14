f48219f00c1a3030465c03f310411f70f934fb42
Albina Startseva <a.komissarova.s@yandex.ru>
2023-07-14 11:18:20 +0300

    fix: исправление ошибок

--- scripts/script.js
+++ scripts/script.js
@@ -1,23 +1,25 @@
 
 
-document.querySelector('.profile__edit-button').addEventListener('click', function () {
-  console.log('click');
-  document.querySelector('.popup').style.display = 'flex';
+document.querySelector('.profile__info-button').addEventListener('click', function () {
+  document.querySelector('.popup').classList.add('popup__opened');
 });
 
 
-document.querySelector('.popup__save-button').addEventListener('click', function ()  {
-    var valName = document.querySelector('.input-name').value;
-    var valJob = document.querySelector('.input-job').value;
 
-    document.querySelector('.profile__info-name').textContent = valName;
-    document.querySelector('.profile__info-description').textContent = valJob;
-    document.querySelector('.popup').style.display = 'none';
-}); 
-
-
-document.querySelector('.popup__container-close').addEventListener('click', function() {
-    document.querySelector('.popup').style.display = 'none';
+document.querySelector('.popup__container-close').addEventListener('click', function () {
+  document.querySelector('.popup').classList.remove('popup__opened');
 });
 
 
+let myForm = document.querySelector('form[name="popupForm"]');
+myForm.addEventListener('submit', function (event) {
+  event.preventDefault();
+  let valName = document.querySelector('input[name="firstName"]').value;
+  let valJob = document.querySelector('input[name="job"]').value;
+
+
+  document.querySelector('.profile__info-name').textContent = valName;
+  document.querySelector('.profile__info-description').textContent = valJob;
+  document.querySelector('.popup').classList.remove('popup__opened');
+
+});
